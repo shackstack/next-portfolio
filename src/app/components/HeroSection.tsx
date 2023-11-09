f72cf3493967a1fa5357b644c6ca1df770e4c64c
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-11-09 23:09:49 +0900

    feat: 연락처 추가

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -31,17 +31,22 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href='https://velog.io/@shackstack'
-              className='px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white'>
-              Velog
-            </Link>
-            <Link
               href='https://github.com/shackstack'
-              className='px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3'>
+              className='px-1 inline-block mr-3 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3'>
               <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                 My Github
               </span>
             </Link>
+            <Link
+              href='https://velog.io/@shackstack'
+              className='px-1 inline-block mr-3 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3'>
+              <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+                Velog
+              </span>
+            </Link>
+            <div className='px-6 inline-block mr-3 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white'>
+              +82 10-5258-1305
+            </div>
           </div>
         </motion.div>
         <motion.div
