b607c4723d8db56ae41d2c85c7a0eab8015ecd52
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-02-16 01:02:02 +0900

    refactor: 마크다운 스타일 모듈화

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,52 @@ import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => (
+      <h1 style={{ fontWeight: '800', margin: '24px 0', fontSize: '32px' }}>
+        {children}
+      </h1>
+    ),
+    h2: ({ children }) => (
+      <h2 style={{ fontWeight: '800', margin: '24px 0', fontSize: '28px' }}>
+        {children}
+      </h2>
+    ),
+    h3: ({ children }) => (
+      <h3 style={{ fontWeight: '800', margin: '24px 0', fontSize: '24px' }}>
+        {children}
+      </h3>
+    ),
+    h4: ({ children }) => (
+      <h4 style={{ fontWeight: '800', margin: '24px 0', fontSize: '20px' }}>
+        {children}
+      </h4>
+    ),
+    h5: ({ children }) => (
+      <h5 style={{ fontWeight: '800', margin: '24px 0', fontSize: '18px' }}>
+        {children}
+      </h5>
+    ),
+    a: ({ children }) => (
+      <a style={{ textDecoration: 'underline' }}>{children}</a>
+    ),
+    ul: ({ children }) => (
+      <ul style={{ listStyleType: 'circle', paddingLeft: '24px' }}>
+        {children}
+      </ul>
+    ),
+    code: ({ children }) => (
+      <code
+        style={{
+          color: 'black',
+          backgroundColor: '#e5e7eb',
+          borderRadius: '6px',
+          padding: '1px 4px',
+          margin: '0 2px',
+          counterReset: 'line',
+        }}>
+        {children}
+      </code>
+    ),
     ...components,
   };
 }
